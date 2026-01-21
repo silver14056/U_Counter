02af565327e431593e5c8fc9c7890d9369560668
silver14056 <silver14056@mail.ru>
2026-01-21 21:48:46 +0300

    Edit Scripts

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -7,33 +7,17 @@ using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
-    [SerializeField] private Button _button;
-    [SerializeField] private int _startCout = 7;
+    [SerializeField] private int _startCount = 0;
 
     private int _currentCount;
 
     public event Action<int> Changed;
 
-    public int StartCout => _startCout;
+    public int StartCout => _startCount;
 
     private void Awake()
     {
-        _currentCount = _startCout;
-    }
-
-    private void OnEnable()
-    {
-        _button.onClick.AddListener(OnButtonClicked);
-    }
-
-    private void OnDisable()
-    {
-        _button.onClick.RemoveListener(OnButtonClicked);
-    }
-
-    private void OnButtonClicked()
-    {
-        Count();
+        _currentCount = _startCount;
     }
 
     public void Count()
--- Assets/CounterSwitch.cs
+++ Assets/CounterSwitch.cs
@@ -6,21 +6,25 @@ using UnityEngine.UI;
 public class CounterSwitch : MonoBehaviour
 {
     [SerializeField] private Button _button;
-    //[SerializeField] private Counter _counter;
+    [SerializeField] private Counter _counter;
 
-    //private void OnEnable()
-    //{
-    //    _button.onClick.AddListener(OnButtonClicked);
-    //}
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClicked);
+    }
 
-    //private void OnDisable()
-    //{
-    //    _button.onClick.RemoveListener(OnButtonClicked);
-    //}
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnButtonClicked);
+    }
 
-    //private void OnButtonClicked()
-    //{
-    //    Debug.Log("Switch-Click");
-    //    _counter.Count();
-    //}
+    private void OnButtonClicked()
+    {
+        _counter.Count();
+    }
+
+    private void IncrementCounterSlow()
+    {
+
+    }
 }
